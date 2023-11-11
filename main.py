cd123ba682153e1f2c2ee40e87bffafe5e156620
Pavlo <44544644+Salupol@users.noreply.github.com>
2023-11-11 13:58:36 +0100

    third ver, now info in window is updating, loging is implemented and made "press q to exit"

--- main.py
+++ main.py
@@ -1,47 +1,50 @@
 import paho.mqtt.client as mqtt
 import pygame
 import json
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("tele/main_battery/SENSOR")
-def on_message(client, userdata, msg):
-    data = json.loads(msg.payload)
-    print(data['voltage'])
-    display_temperature(data['voltage'])
 
+
+# Function to write logs to a file
+def write_to_log(message):
+    with open('data.log', 'a') as log_file:
+        log_file.write(message + '\n')
+
+
+# Function to update the temperature display in Pygame
 def display_temperature(voltage):
-    # Initialize pygame
-    pygame.init()
-
-    # Create a window
-    screen = pygame.display.set_mode((800, 800))
-    pygame.display.set_caption('Info from MQTT')
-
     # Set up fonts
     font = pygame.font.Font(None, 36)
+
+    # Draw the text on the screen
+    screen.fill((0, 0, 0))
     text = font.render(f'Voltage: {voltage}', True, (255, 255, 255))
-    #printing "press q to quit"
     text2 = font.render(f'Press q to quit', True, (255, 255, 255))
+    screen.blit(text, (50, 50))
+    screen.blit(text2, (500, 700))
+    pygame.display.flip()
 
 
-    # Main game loop
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                exit()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-                running = False
-                exit()
-        # Draw the text on the screen
-        screen.fill((0, 0, 0))
-        screen.blit(text, (50, 50))
-        screen.blit(text2, (500, 700 ))
-        pygame.display.flip()
+def on_connect(client, userdata, flags, rc):
+    write_to_log("Connected with result code " + str(rc))
+    print("Connected with result code " + str(rc))
+    client.subscribe("tele/main_battery/SENSOR")
 
-    # Quit pygame
-    pygame.quit()
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload)
+    voltage = data.get('voltage')
+
+    if voltage is not None:
+        write_to_log(f"Received voltage: {voltage}")
+        print(f"Received voltage: {voltage}")
+        display_temperature(voltage)
+
+
+# Initialize pygame and create a window
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Info from MQTT')
+
+# Initialize MQTT client
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -49,9 +52,23 @@ username = "owntracks"
 password = "zhopa"
 client.username_pw_set(username, password)
 client.connect("192.168.0.106", 8883, 60)
-client.loop_forever()
 
+# Main game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            running = False
+            exit()
 
+    # Continue the Pygame display update
+    pygame.display.flip()
 
+    # Continue the MQTT loop
+    client.loop(0.1)  # Add a short delay to avoid high CPU usage
 
-
+# Quit pygame
+pygame.quit()
