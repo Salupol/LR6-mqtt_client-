7aa25216811e6a7af81043c8721fd6b49b232863
Pavlo <44544644+Salupol@users.noreply.github.com>
2023-11-11 13:40:58 +0100

    second ver, nothing totally new

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import paho.mqtt.client as mqtt
 import pygame
 import json
-import threading
-import time
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("tele/main_battery/SENSOR")
@@ -11,18 +9,20 @@ def on_message(client, userdata, msg):
     print(data['voltage'])
     display_temperature(data['voltage'])
 
-
 def display_temperature(voltage):
     # Initialize pygame
     pygame.init()
 
     # Create a window
-    screen = pygame.display.set_mode((400, 200))
-    pygame.display.set_caption('Temperature Display')
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption('Info from MQTT')
 
     # Set up fonts
     font = pygame.font.Font(None, 36)
-    text = font.render(f'Temperature: {voltage}', True, (255, 255, 255))
+    text = font.render(f'Voltage: {voltage}', True, (255, 255, 255))
+    #printing "press q to quit"
+    text2 = font.render(f'Press q to quit', True, (255, 255, 255))
+
 
     # Main game loop
     running = True
@@ -30,10 +30,14 @@ def display_temperature(voltage):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+                exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                running = False
+                exit()
         # Draw the text on the screen
         screen.fill((0, 0, 0))
         screen.blit(text, (50, 50))
+        screen.blit(text2, (500, 700 ))
         pygame.display.flip()
 
     # Quit pygame
